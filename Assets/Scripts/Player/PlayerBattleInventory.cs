9231e58bf4f0aeaa96276af5d881cfb25599b855
Ama38 <128802670+Ama38@users.noreply.github.com>
2024-04-27 23:07:03 +0500

    Changes in inventory sctructure

--- Assets/Scripts/Player/PlayerBattleInventory.cs
+++ Assets/Scripts/Player/PlayerBattleInventory.cs
@@ -6,18 +6,6 @@ using UnityEngine;
 public class PlayerBattleInventory : ScriptableObject
 {
     // fazik vse polya zdes doljni bit public ok da
-    public GameObject Slot1;// inventar slot odin
-    public int Slot1Quantity;// kolvo unitov v pervom slote
-    public GameObject Slot2;
-    public int Slot2Quantity;
-    public GameObject Slot3;
-    public int Slot3Quantity;
-    public GameObject Slot4;
-    public int Slot4Quantity;
-    public GameObject Slot5;
-    public int Slot5Quantity;
-    public GameObject Slot6;
-    public int Slot6Quantity;
-    public GameObject Slot7;
-    public int Slot7Quantity;
+    public GameObject[] slots = new GameObject[7];
+    public int[] numberOfUnits = new int[7];    
 }
